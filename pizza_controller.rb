51ba64545c6b496edc0d2f7193957d9b6d5c098c
sarahmurphy86 <sarah.graham86@gmail.com>
2018-05-02 21:26:17 +0100

    update order working

--- pizza_controller.rb
+++ pizza_controller.rb
@@ -4,43 +4,47 @@ require( 'pry-byebug' )
 
 require_relative('./models/pizza_order.rb')
 
-# READ orders
+# read all orders
 get '/pizza_orders' do
   @orders = PizzaOrder.all()
   erb(:index)
 end
 
-# CREATE orders
+# new order
 get '/pizza_orders/new' do
   erb(:new)
 end
 
-# new
+# show order
+get '/pizza_orders/:id' do
+  @order = PizzaOrder.find(params[:id])
+  erb(:show)
+end
+
+# create order
 post'/pizza_orders'do
   @order = PizzaOrder.new(params)
   @order.save()
   erb(:create)
 end
 
-# SHOW order
-get '/pizza_orders/:id' do
-  @order = PizzaOrder.find(params[:id])
-  erb(:show)
-end
 
-
-# UPDATE existing orders
+# edit existing orders
 get '/pizza_orders/:id/edit' do
   @order = PizzaOrder.find(params[:id])
-  @order.update()
-  redirect to ('pizza_order')
+  erb(:edit)
 end
 
+#update the order
+post '/pizza_orders/:id' do # update
+  PizzaOrder.new( params ).update
+  redirect to '/pizza_orders'
+end
 
-
-# DELETE orders
+#delete the order
 post '/pizza_orders/:id/delete' do
   @order = PizzaOrder.find(params[:id])
   @order.delete()
-  redirect to('/pizza_orders')
+  erb(:delete)
+  # redirect to('/pizza_orders')
 end
